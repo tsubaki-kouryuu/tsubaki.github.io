f2f8f2979ef5c17bf350d55bd024985aa279a01b
tsubaki-kouryuu <tsubaki-kouryuu@users.noreply.github.com>
2023-05-02 22:58:26 +0000

    deploy: f131379e6ccb926706fc4012545b18fc48c31ae5

---
+++ _next/static/kVu2FbzIwhqgF5hnH9myx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-91df2a1167b3f46f.js","static/chunks/pages/index-55b2bde82194d25a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
