5c1a307df018bbe694135bc0a731fdc899a38960
Tsubaki Kouryuu <tsubaki@kouryuu.me>
2023-05-02 15:51:56 -0700

    potentially get github to correctly build and deploy got github pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? '/tsubaki.github.io/' : '',
   reactStrictMode: true,
   output: 'export',
   images: {
