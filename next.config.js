f93fe9f128ed4e79ae7dd213d4aeb17a8fe2c524
Tsubaki Kouryuu <tsubaki@kouryuu.me>
2023-05-02 18:34:53 -0700

    fix image display on github pages (again) (again)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? '/tsubaki.github.io/' : '',
+  assetPrefix: isProd ? '' : '',
   reactStrictMode: true,
   output: 'export',
   images: {
