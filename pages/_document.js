158af38190a66ac20c1c185376ceb4c7ee44c829
Tsubaki Kouryuu <tsubaki@kouryuu.me>
2023-05-02 18:52:24 -0700

    adding title and meta

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" className="dark" style={{colorScheme: 'dark'}}>
-      <Head />
+      <Head>
+        <title>Tsubaki Kouryuu</title>
+        <meta name='description' content='a simple Auri programmer' />
+      </Head>
       <body className={`bg-gray-950 overflow-hidden`}>
         <Main />
         <NextScript />
